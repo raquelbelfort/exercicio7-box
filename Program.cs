81a6b6f9e402cba899d9a58d9e30506fd2ec176e
Raquel Belfort <raquelzinha.belfort@gmail.com>
2020-09-07 17:48:58 -0300

    Adição de resolução.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercicio7_box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		    Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("+----------------+");
+		    Console.WriteLine("| Raquel Belfort |");
+            Console.WriteLine("+----------------+");        }
+    }
+}
